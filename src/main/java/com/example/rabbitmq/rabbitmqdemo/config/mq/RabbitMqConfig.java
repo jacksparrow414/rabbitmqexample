cbc1ac6e39eb3fa784172eba5e7c17dc213ba2de
Serendipity <jacksparrow414@163.com>
2020-12-20 21:54:08 +0800

    exchange pom文件修改、统一版本和消费者端，添加注释

--- src/main/java/com/example/rabbitmq/rabbitmqdemo/config/mq/RabbitMqConfig.java
+++ src/main/java/com/example/rabbitmq/rabbitmqdemo/config/mq/RabbitMqConfig.java
@@ -1,8 +1,9 @@
 package com.example.rabbitmq.rabbitmqdemo.config.mq;
 
 import com.rabbitmq.client.ConnectionFactory;
+import java.util.Collections;
 import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CompositeChannelListener;
 import org.springframework.amqp.rabbit.connection.PooledChannelConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -30,18 +31,29 @@ public class RabbitMqConfig {
         result.setUsername("dhb");
         result.setPassword("123456");
         result.setVirtualHost("dhb");
+        // 设置连接的名字
         result.setConnectionNameStrategy(factory -> "producer-connection");
         // TODO 对象池的设置
         // 使用Apache Common Pool2,一共有两个对象池
         result.setPoolConfigurer((pool, tx) -> {
             if (tx) {
                 // 用于管理事务的对象池
-                pool.setMaxTotal(2);
+                pool.setMaxTotal(8);
             }else {
                 // 非事务的对象池,默认最大为8
                 pool.setMaxTotal(8);
+                // 以下四行能够使驱逐规则生效，至于为什么生效见{@link org.apache.commons.pool2.impl.DefaultEvictionPolicy}代码
+                // 不要开启驱逐规则
+//                pool.setMaxIdle(2);
+//                pool.setMinIdle(2);
+//                pool.setMinEvictableIdleTimeMillis(20000);
+//                pool.setSoftMinEvictableIdleTimeMillis(15000);
+                // 驱逐线程执行扫描任务周期，设置为10秒一次
+//                pool.setTimeBetweenEvictionRunsMillis(10000);
+//                pool.setNumTestsPerEvictionRun(5);
             }
         });
+        result.setChannelListeners(Collections.singletonList(new CompositeChannelListener()));
         return result;
     }
     
@@ -57,12 +69,7 @@ public class RabbitMqConfig {
     @Bean(name = "rabbitTemplate")
     public RabbitTemplate rabbitTemplate() {
         RabbitTemplate reuslt = new RabbitTemplate(connectionFactory());
-//        reuslt.addListener();
-        
         reuslt.setMessageConverter(new Jackson2JsonMessageConverter());
-//        reuslt.addBeforePublishPostProcessors();
-//        reuslt.addAfterReceivePostProcessors();
-//        reuslt.setConfirmCallback();
         return reuslt;
     }
 }
--- src/test/java/com/example/rabbitmq/rabbitmqdemo/example/TopicExampleTest.java
+++ src/test/java/com/example/rabbitmq/rabbitmqdemo/example/TopicExampleTest.java
@@ -3,11 +3,8 @@ package com.example.rabbitmq.rabbitmqdemo.example;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author jacksparrow414
